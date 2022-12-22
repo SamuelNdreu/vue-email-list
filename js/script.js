8bedd87da58d8efbbd22b258d2ebf5a29db473c6
SamuelNdreu <samwellsoficcial@gmail.com>
2022-12-22 14:09:53 +0100

    finished exercise 21/12/2022

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            emails: [],
+        }
+    },
+
+    methods: {
+        getEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((response) => {
+                        this.emails.push(response.data.response)
+                    })
+            }
+        }
+    },
+
+    created() {
+        this.getEmails()
+    }
+
+}).mount('#app')
